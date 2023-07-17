3ae056cc67e868f2a1f52e9ffe65c4502fcabbcc
Derek <derekdanielson84@gmail.com>
2023-07-17 09:19:34 -0600

    Notes added

---
+++ app.js
@@ -0,0 +1,11 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = false;
+
+        if (!error) {
+            resolve({ name: 'John', age: 30 });
+        } else {
+            reject('Error: Something went wrong');
+        }
+    }, 1000);
+});
